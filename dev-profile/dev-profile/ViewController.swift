47c33a73bcf644af157c0f67b4fea75fba3ce334
Shone <shonesrbija@yahoo.com>
2019-06-06 03:25:30 +0200

    pocetno punjenje iz xCode-a

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by MacBook Pro on 6/6/19.
+//  Copyright © 2019 MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
